033a8ca1eccbc868488b292f9164d330de5b3a3c
Ykrekoten <gyliungly17@gmail.com>
2024-01-08 22:05:28 +0300

    Stub and tests for add

--- src/main/java/netology/PhoneBook.java
+++ src/main/java/netology/PhoneBook.java
@@ -4,6 +4,16 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class PhoneBook {
+    private Map<String, String> contacts;
+
+    public PhoneBook() {
+        this.contacts = new HashMap<>();
+    }
+
+    public int add(String name, String number) {
+        contacts.put(name, number);
+        return 0;
+    }
 
     }
 
--- src/test/java/PhoneBookTest.java
+++ src/test/java/PhoneBookTest.java
@@ -2,8 +2,16 @@ import netology.PhoneBook;
 import org.junit.jupiter.api.Assertions;
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
+
 
 public class PhoneBookTest {
-    
+    @Test
+    public void testAdd() {
+        PhoneBook phoneBook = new PhoneBook();
+        assertEquals(1, phoneBook.add("Alice", "1234567890"));
+        assertEquals(2, phoneBook.add("Bob", "9876543210"));
+        assertEquals(3, phoneBook.add("Charlie", "5555555555"));
+    }
 
 }
